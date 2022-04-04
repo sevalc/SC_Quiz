2bdc8b1a396982ca6108735c05b12917cfa7eaf8
Seval Cakir <sevalc@live.com>
2022-04-03 20:40:22 -0700

    initial html and js pseudocode

---
+++ script.js
@@ -0,0 +1,7 @@
+//create an array with multiple questions. each has 4 choices 
+//listen for click on start button
+//if answer is correct 
+    //print coorect on screen
+    //add to score 
+//show final score when all q answered / timer reached 0
+//save initials  
