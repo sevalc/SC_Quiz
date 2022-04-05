af3b255cde2e9a5819ec0982b01938e1a802612c
Seval Cakir <sevalc@live.com>
2022-04-04 17:48:15 -0700

    question array and CSS styling added

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
 //create an array with multiple questions. each has 4 choices 
+var questionBank = [ 
+{
+    question1: "What does HTML stand for?",
+    answer1: {
+        a: "Hyper Trainer Marking Language",
+        b: "Hyper Text Marketing Language",
+        c: "Hyper Text Markup Language",
+        d: "Hyper Text Markup Leveler"
+    },
+    correctAnswer: "c"
+},
+    {   
+    question2: "What is the type of loop that continues through a block of code as long as the specified condition remains TRUE?",
+    answer2: {
+        a: "Conditional Loop", 
+        b: "For Loop", 
+        c: "Else Loop", 
+        d: "While Loop",
+    },
+    correctAnswer: "d"
+},
+    {
+    Question3: "What is considered to be the most popular programming language in the world?",
+    answer3: {
+        a: "HTML",
+        b: "Ruby", 
+        c: "Javascript",
+        d: "None of the above"
+    },
+    correctAnswer: "c" 
+];
+
 //listen for click on start button
+
+    //start timer / countdown from 60
 //if answer is correct 
-    //print coorect on screen
+    //print correct on screen
     //add to score 
+//if answer is incorrect 
+    //print incorrect on the screen 
 //show final score when all q answered / timer reached 0
-//save initials  
+//save initials and score 
